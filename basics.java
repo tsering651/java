629163f914c3d082cf8d3b85e55c31ae7f016927
tsering651 <tseringwang4039@gmail.com>
2022-01-14 19:49:08 +0530

    writing my feedback

--- basics.java
+++ basics.java
@@ -4,4 +4,13 @@
 
 public class java{
 public static void main(String[] args){
-System.out.println("i am tsering wangchus robot"); } }
+System.out.println("i am tsering wangchus robot"); 
+int a=9;
+int b=10;
+int c=a+b;
+System.out.print(c);} }
+
+
+
+as i am proceeding forward in the moduke of git and giyhub i am understanding all the things that once seem to be very 
+difficult
